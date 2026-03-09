96cb70ca39ee6182cf412ae3505e854758a91168
fuisl <135719107+fuisl@users.noreply.github.com>
2026-03-09 11:55:38 +0900

    feat: PettingZoo wrapper.

--- marl_env/__init__.py
+++ marl_env/__init__.py
@@ -1,4 +1,5 @@
 from marl_env.sumo_env import TrafficSignalEnv
+from marl_env.pettingzoo_env import SumoTrafficParallelEnv
 from marl_env.traci_adapter import TraCIAdapter
 from marl_env.graph_builder import GraphBuilder
 from marl_env.reward import RewardCalculator
@@ -6,6 +7,7 @@ from marl_env.action_constraints import ActionConstraints
 
 __all__ = [
     "TrafficSignalEnv",
+    "SumoTrafficParallelEnv",
     "TraCIAdapter",
     "GraphBuilder",
     "RewardCalculator",
